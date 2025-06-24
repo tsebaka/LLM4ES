121071a6af491ecd47bd506aeeb8a5f65a9153c4
Omar Zoloev <zoloevomarwork@gmail.com>
2025-06-24 11:38:03 +0300

    Create train.sh

---
+++ scripts/train.sh
@@ -0,0 +1,12 @@
+WORK_DIR=$HOME/zoloev-city/gigachat
+CONFIG_DIR=$WORK_DIR/source/llm-foundry/scripts/train/yamls/pretrain
+
+source $WORK_DIR/source/llm-foundry/llmfoundry-venv/bin/activate
+
+export WANDB_API_KEY=2736e3a99574e3049342cd33a3154aa307a08aa1
+export WANDB_PROJECT="gigachat"
+export WANDB_DIR=$WORK_DIR/checkpoints
+
+composer $WORK_DIR/source/llm-foundry/scripts/train/train.py \
+    $CONFIG_DIR/$CONFIG \
+    variables.work_dir=$WORK_DIR
