d504f7192706faf2f430dcdfda29d9b88c4ffc43
Omar Zoloev <zoloevomarwork@gmail.com>
2025-06-24 11:38:49 +0300

    Create inference.sh

---
+++ scripts/inference.sh
@@ -0,0 +1,13 @@
+WORK_DIR=$HOME/zoloev-city/gigachat
+CONFIG_DIR=$WORK_DIR/source/llm-foundry/scripts/train/yamls/pretrain
+
+source $WORK_DIR/source/llm-foundry/llmfoundry-venv/bin/activate
+
+export WANDB_API_KEY=2736e3a99574e3049342cd33a3154aa307a08aa1
+export WANDB_PROJECT="gigachat"
+export WANDB_DIR=$WORK_DIR/checkpoints
+
+accelerate launch $WORK_DIR/source/llm4trx/inference.py \
+    --config-dir $CONFIG_DIR \
+    --config-name $CONFIG \
+    variables.work_dir=$WORK_DIR
